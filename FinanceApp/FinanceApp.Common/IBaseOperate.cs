e54f5541ded0b0b6c1fbdb9057d2def543baa42a
layupbolon <20732904@qq.com>
2015-08-25 15:44:41 +0800

    添加

---
+++ FinanceApp/FinanceApp.Common/IBaseOperate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FinanceApp.Common
+{
+    public interface IBaseOperate<T>
+    {
+        T Add(T entity);
+
+        int Count(Expression<Func<T, bool>> predicate);
+
+        bool Update(T entity);
+
+        bool Invalid(T entity);
+
+        bool Exist(Expression<Func<T, bool>> predicate);
+
+        T Find(Expression<Func<T, bool>> predicate);
+
+        IQueryable<T> FindList<S>(Expression<Func<T, bool>> whereLambda, bool isAsc, Expression<Func<T, S>> orderLambda);
+
+        IQueryable<T> FindPageList<S>(int pageIndex, int pageSize, out int totalRecord,
+            Expression<Func<T, bool>> whereLambda, bool isAsc, Expression<Func<T, S>> orderLambda);
+    }
+}
