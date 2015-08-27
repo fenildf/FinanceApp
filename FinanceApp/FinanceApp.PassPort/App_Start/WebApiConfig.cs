43d8755d9712741e299e52692594884750164b99
layupbolon <20732904@qq.com>
2015-08-27 14:44:47 +0800

    添加用户认证中心、登录界面

---
+++ FinanceApp/FinanceApp.PassPort/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace FinanceApp.PassPort
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FinanceApp/FinanceApp.PassPort/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinanceApp.PassPort.Models
+{
+    public class HomeViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
