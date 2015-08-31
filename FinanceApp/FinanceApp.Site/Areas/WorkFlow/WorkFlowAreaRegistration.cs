f0886ba8c5d68152f845beafdadf2327af964b66
layupbolon <20732904@qq.com>
2015-08-31 14:56:19 +0800

    添加区域文件夹

---
+++ FinanceApp/FinanceApp.Site/Areas/WorkFlow/WorkFlowAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FinanceApp.Site.Areas.WorkFlow
+{
+    public class WorkFlowAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WorkFlow";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WorkFlow_default",
+                "WorkFlow/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
